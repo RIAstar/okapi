226068c39fe346c8b2bd604da9108350f80f2614
RIAstar <maxime@riastar.net>
2025-04-23 22:58:19 +0200

    feat(open-api-parser): simplified parser and added tests for external $ref resolution

--- packages/open-api-parser/src/parse-spec.ts
+++ packages/open-api-parser/src/parse-spec.ts
@@ -1,25 +1,12 @@
-import { readFile } from 'node:fs/promises';
-
-import { parse } from 'yaml';
 import { $RefParser } from '@apidevtools/json-schema-ref-parser';
 import type { JSONSchema } from '@apidevtools/json-schema-ref-parser';
 
 
-export const parseSpec = (filePath: string): Promise<JSONSchema> => {
-    if (filePath.endsWith('.json')) return parseJson(filePath);
-    if (filePath.endsWith('.yml') || filePath.endsWith('.yaml')) return parseYaml(filePath);
-    return Promise.reject(new Error(`Input file "${filePath}" is neither y(a)ml nor json`));
-}
+const validExtensions = ['.json', '.yml', 'yaml'];
 
-const resolve = (json: unknown) => $RefParser.dereference(json);
-
-const parseJson = (filePath: string) =>
-    readFile(filePath, 'utf8')
-        .then(JSON.parse)
-        .then(resolve);
-
-const parseYaml = (filePath: string) =>
-    readFile(filePath, 'utf8')
-        .then(parse)
-        .then(resolve);
-
+export const parseSpec = (filePath: string): Promise<JSONSchema> =>
+    validExtensions.some(ext => filePath.endsWith(ext))
+        ? $RefParser
+            .parse(filePath)
+            .then(json => $RefParser.dereference(json))
+        : Promise.reject(new Error(`Input file "${filePath}" is neither y(a)ml nor json`));
